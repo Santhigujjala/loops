e1c443230f5b15a6097fc9893134502cba22a90e
Santhigujjala <115785993+Santhigujjala@users.noreply.github.com>
2022-10-26 14:20:44 +0530

    Add files via upload

---
+++ 33 pattern.py
@@ -0,0 +1,19 @@
+# E D C B A 
+# F E D C 
+# G F E 
+# H G 
+# I 
+
+
+i=65
+k=69
+while i<=69:
+    j=69
+    a=k
+    while j>=i:
+        print(chr(a),end=" ")
+        j=j-1
+        a=a-1
+    k=k+1
+    i=i+1
+    print()
