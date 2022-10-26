a980f43bdf37a7d595eeb71a2bc521f0d9dca7c8
Santhigujjala <115785993+Santhigujjala@users.noreply.github.com>
2022-10-26 14:21:45 +0530

    Add files via upload

---
+++ 31 pattern.py
@@ -0,0 +1,16 @@
+# 12345
+# 12345
+# 12345
+# 12345
+# 12345
+
+
+n=int(input("enter the number"))
+i=1
+while i<=n:
+    j=1
+    while j<=n:
+        print(j,end="")
+        j=j+1
+    i=i+1
+    print()
