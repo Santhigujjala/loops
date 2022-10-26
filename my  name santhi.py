0e52bb1f6f4c343d533eba9e5c59da3e45530787
Santhigujjala <115785993+Santhigujjala@users.noreply.github.com>
2022-10-26 14:36:53 +0530

    Add files via upload

---
+++ my  name santhi.py
@@ -0,0 +1,17 @@
+from unicodedata import name
+from yaml import MarkedYAMLError
+
+
+# My
+# name
+# sathi
+
+a="my name santhi"
+i=0
+while i<len(a):
+    if a[i]==" ":
+        print()
+    else:
+        print(a[i],end="")
+    i=i+1
+print()
