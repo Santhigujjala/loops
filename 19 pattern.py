97c1899462c3bc1fbcc290bbe8dfefc3aeb28b50
Santhigujjala <115785993+Santhigujjala@users.noreply.github.com>
2022-10-26 14:28:16 +0530

    Add files via upload

---
+++ 19 pattern.py
@@ -0,0 +1,19 @@
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+
+i=1
+while i<=5:
+    k=0
+    while k<=5-i:
+        print("",end="")
+        k=k+1
+    j=1
+    while j<=i:
+        print(i,end="")
+        j=j+1
+    i=i+1
+    print()
