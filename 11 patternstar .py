c405eef482b48d3df7130163522b22c22eeaeaf3
Santhigujjala <115785993+Santhigujjala@users.noreply.github.com>
2022-10-26 14:31:03 +0530

    Add files via upload

---
+++ 11 patternstar .py
@@ -0,0 +1,19 @@
+#  * * * * * 
+#   * * * * 
+#    * * * 
+#     * * 
+#      * 
+
+
+i=5
+while i>=1:
+    k=0
+    while k<=5-i:
+        print(" ",end="")
+        k=k+1
+    j=1
+    while j<=i:
+        print("*",end=" ")
+        j=j+1
+    i=i-1
+    print()
