e4b6c5d250b8f06e484dc9b6a5f5dd729bbe027e
Santhigujjala <115785993+Santhigujjala@users.noreply.github.com>
2022-10-26 14:38:57 +0530

    Add files via upload

---
+++ 3 exam.py
@@ -0,0 +1,12 @@
+# n=int(input("enter the number"))
+# k=84+1
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=n:
+#         print(chr(k),end=" ")
+#         k=k+1
+#         j=j+1
+#     k=k-10
+#     i=i+1
+#     print()
